57661477d44b78ed994248d1e6a9f8ee6ecf922f
Lukas Metlicka <lmetlicka@g.harvard.edu>
2014-07-08 15:53:38 -0400

    my first commit

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+		<link rel="text/css" href="style/style.css">
+		<link rel="text/javascript" href="script.js">
+		<?php require('logic.php') ?>
+	</head>
+	
+	<body> 
+		<div class="scott"></div>
+	</body>
+</html>
